0d4a1073504a2590f4abf2b02acda0cb12732a16
Inbum Chong <ibchong98@gmail.com>
2022-01-17 20:24:37 +0800

    new file

--- Reference/problems.py
+++ Reference/problems.py
@@ -66,13 +66,3 @@ elif parameter == 't':
     window.resizable(False, False)
 
 
-
-
-
-# if problem == "All":
-#     print(problems_set)
-# else:
-#     if problem not in problems_set.keys():
-#         print("No such problem number exists")
-#     else:
-#         print('Summary: {}\nCategory: {}\nURL: {}\nPlatform: {}\nDifficulty: {}'.format(problems_set[problem][0], problems_set[problem][1], problems_set[problem][2], problems_set[problem][3], problems_set[problem][4]))
