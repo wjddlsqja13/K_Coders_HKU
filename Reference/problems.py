a98deebfee6990b78cbfd171863cdcb63c60e7c8
Inbum Chong <ibchong98@gmail.com>
2022-01-17 16:11:19 +0800

    problems.py changed

--- Reference/problems.py
+++ Reference/problems.py
@@ -1,27 +1,78 @@
 import sys
+import tkinter
 
-problem = sys.argv[1]
+from tkinter import ttk
+from problems_DB import problems_set
+
+parameter = sys.argv[1]
 
 if len(sys.argv) != 2:
-    print("Please Input Problem No.")
+    print("Please Input a Parameter: p / t")
     sys.exit()
 
+if parameter == 'p':
+
+    window = tkinter.Tk()
+    window.title("Main")
+    window.geometry('500x350')
+    window.resizable(False, False)
+
+    def buttonClicked():
+        labelNew.configure(text='Problem Number: ' + name.get())
+        description.configure(text='Title: ' + problems_set[name.get()][0])
+        topic.configure(text='Topic: ' + problems_set[name.get()][1])
+        link.configure(text='Link: ' + problems_set[name.get()][2])
+        source.configure(text='Source: ' + problems_set[name.get()][3])
+        difficulty.configure(text='Difficulty: ' + problems_set[name.get()][4])
+
+    label = ttk.Label(window, text = "Input the problem no. (ex. H12345)")
+    label.grid(column=0, row=0)
+
+    button = ttk.Button(window, text="click", command=buttonClicked)
+    button.grid(column=1, row=2)
+
+    name = tkinter.StringVar()
+    textbox = ttk.Entry(window, width=24, textvariable=name)
+    textbox.grid(column=0, row=2)
+
+    labelNew = ttk.Label(window, text="")
+    labelNew.grid(column=0, row=3)
+    
+    space = ttk.Label(window, text="")
+    space.grid(column=0, row=4)
+
+    description = ttk.Label(window, text="")
+    description.grid(column=0, row=5)
+
+    topic = ttk.Label(window, text="")
+    topic.grid(column=0, row=6)
+
+    link = ttk.Label(window, text="")
+    link.grid(column=0, row=7)
+
+    source = ttk.Label(window, text="")
+    source.grid(column=0, row=8)
+
+    difficulty = ttk.Label(window, text="")
+    difficulty.grid(column=0, row=9)
+
+    window.mainloop()
+
+elif parameter == 't':
+
+    window = tkinter.Tk()
+    window.title("Main")
+    window.geometry('500x350')
+    window.resizable(False, False)
 
 
 
-problems_set = {
-    #문제코드 : [문제 요약, 카테고리, 링크, 출처, 난이도]
-    'H123456':['Defining', 'BFS', 'http...', 'HakerRank', 'Hard']
-}
 
 
-
-
-
-if problem == "All":
-    print(problems_set)
-else:
-    if problem not in problems_set.keys():
-        print("No such problem number exists")
-    else:
-        print('Summary: {}\nCategory: {}\nURL: {}\nPlatform: {}\nDifficulty: {}'.format(problems_set[problem][0], problems_set[problem][1], problems_set[problem][2], problems_set[problem][3], problems_set[problem][4]))
+# if problem == "All":
+#     print(problems_set)
+# else:
+#     if problem not in problems_set.keys():
+#         print("No such problem number exists")
+#     else:
+#         print('Summary: {}\nCategory: {}\nURL: {}\nPlatform: {}\nDifficulty: {}'.format(problems_set[problem][0], problems_set[problem][1], problems_set[problem][2], problems_set[problem][3], problems_set[problem][4]))
---
+++ Reference/problems_DB.py
@@ -0,0 +1,5 @@
+problems_set = {
+    #문제코드 : [문제 요약, 토픽, 링크, 출처, 난이도]
+    'H12345':['Defining', 'BFS', 'http...', 'HakerRank', 'Hard'],
+    'K12345':['Sorting', 'Sort', 'http...', 'LeetCode', 'Easy']
+}
