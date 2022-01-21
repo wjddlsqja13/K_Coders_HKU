dbe961a486daa043abccdf2e87a68c907448bbfc
Inbum Chong <ibchong98@gmail.com>
2022-01-21 15:25:31 +0800

    new updates

--- Reference/problems_DB.py
+++
@@ -1,5 +0,0 @@
-problems_set = {
-    #문제코드 : [문제 요약, 토픽, 링크, 출처, 난이도]
-    'GDA001':['Defining', 'Array', 'https://practice.geeksforgeeks.org/problems/largest-element-in-array4009/1/?category[]=Arrays&category[]=Arrays&page=1&query=category[]Arrayspage1category[]Arrays', 'GFG', 'Easy'],
-    'K12345':['Sorting', 'Sort', 'http...', 'LeetCode', 'Easy']
-}
