56a307ba6e71ea4bbda469d405e9d15972cc8d43
yeonpark <pyeon99@gmail.com>
2022-01-23 22:27:39 +0900

    Update HDA001_Yeon.py

--- Team_B/Solutions/Data_Structure/1. Array/HDA001_Yeon.py
+++ Team_B/Solutions/Data_Structure/1. Array/HDA001_Yeon.py
@@ -14,14 +14,13 @@ import sys
 #
 
 def hourglassSum(arr):
-    # Write your code here\
     # Write your code here
     results = []
     n = 6
     for i in range(n):
         for j in range(n):
             result = 0
-            if i-1<n-1 or i+1>5 or j-1<0 or j+1>n-1:
+            if i-1<0 or i+1>n-1 or j-1<0 or j+1>n-1:
                 continue
             else:
                 result = sum(arr[i-1][j-1:j+2]) + arr[i][j] + sum(arr[i+1][j-1:j+2])
