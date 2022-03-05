85f1b54c52cfd9cccf4b40c634db7b466abc37ab
suechoii <jungan26@connect.hku.hk>
2022-03-05 21:15:07 +0800

    5 Mar_HAG001

---
+++ Team_B/Solutions/Algorithm/4. Greedy Algorithm/HAG001_Jungan.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    # greedy? 
+    arr = sorted(arr)
+    minimum = sys.maxsize
+    for i in range(len(arr)-1):
+        val = arr[i+1]-arr[i]
+        if val < minimum :
+            minimum = val
+    return minimum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
