a1b917340020a79532308555f8f172fcd56115b2
suechoii <jungan26@connect.hku.hk>
2022-03-05 21:31:03 +0800

    5 Mar_HAG002

---
+++ Team_B/Solutions/Algorithm/4. Greedy Algorithm/HAG002_Jungan.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'luckBalance' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. 2D_INTEGER_ARRAY contests
+#
+
+def luckBalance(k, contests):
+    # Write your code here
+    contests.sort(reverse=True)
+    one_count = 0
+    output = 0
+    for c in contests:
+        if c[1] == 1 : 
+            one_count +=1
+            if one_count <= k :
+                output += c[0]
+            else :
+                output -= c[0]
+        else:
+            output += c[0]
+    return output
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    contests = []
+
+    for _ in range(n):
+        contests.append(list(map(int, input().rstrip().split())))
+
+    result = luckBalance(k, contests)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
